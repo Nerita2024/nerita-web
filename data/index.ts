9be5aad9efcc9b209ee10b6bd3e1faa7991c8f44
Nerita2024 <mstancik@nerita.eu>
2024-07-22 14:27:15 +0200

    Index. ts update

--- data/index.ts
+++ data/index.ts
@@ -206,7 +206,7 @@ export const socialMedia = [
   {
     name: "GitHub",
     img: "/git.svg",
-    link: "https://github.com/",
+    link: "https://github.com/Nerita2024",
   },
   {
     name: "Twitter",
