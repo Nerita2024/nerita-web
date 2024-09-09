ac327557c1a4688444ded694a8a8ef7a8ebe836f
Nerita2024 <mstancik@nerita.eu>
2024-09-09 13:20:38 +0200

    bugfix

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ import { i18n } from './next-i18next.config.mjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n:{
+    locales: ['en', 'sk'],
     defaultLocale: 'en',
     localeDetection: false, 
   },
